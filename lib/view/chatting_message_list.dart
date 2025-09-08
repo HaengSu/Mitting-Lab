8690bc7bc2506b196758241e32ad226a4cf58d5b
HaengSu <onse3840@gmail.com>
2025-09-08 22:02:26 +0900

    feature : chatting ui 구현중

--- lib/view/chatting_message_list.dart
+++ lib/view/chatting_message_list.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ChattingMessageList extends StatefulWidget {
-  const ChattingMessageList({super.key});
+  final List<String> message;
+
+  const ChattingMessageList({required this.message, super.key});
 
   @override
   State<ChattingMessageList> createState() => _ChattingMessageListState();
 }
 
 class _ChattingMessageListState extends State<ChattingMessageList> {
+  final ScrollController _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: )
+    return Expanded(
+        child: ListView.builder(
+            itemCount: widget.message.length,
+            controller: _scrollController,
+            itemBuilder: (context, index) {
+              return ListTile(title: Text(widget.message[index]));
+            }));
   }
 }
--- lib/view/chatting_page.dart
+++ lib/view/chatting_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mitting_lab/view/chatting_message_list.dart';
 
 class ChattingPage extends StatefulWidget {
   const ChattingPage({super.key});
@@ -8,14 +9,16 @@ class ChattingPage extends StatefulWidget {
 }
 
 class _ChattingPageState extends State<ChattingPage> {
+  final List<String> _dummyMessage = ['안녕','이굠댕'];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(padding: EdgeInsets.only(top: 50, left: 20, right: 20),
       child: Column(
         children: [
-          Row(
+          const Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Icon(Icons.arrow_back_ios_new),
@@ -25,7 +28,7 @@ class _ChattingPageState extends State<ChattingPage> {
           ),
           SizedBox(height: 10),
           Divider(),
-          
+          ChattingMessageList(message: _dummyMessage,)
         ],
       ),),
     );
