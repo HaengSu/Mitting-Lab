9a071fdbc8ae9f1bf700283b08f19385bd60d580
HaengSu <onse3840@gmail.com>
2025-09-08 21:51:37 +0900

    feature : chatting ui 구현중

---
+++ lib/view/chatting_message_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChattingMessageList extends StatefulWidget {
+  const ChattingMessageList({super.key});
+
+  @override
+  State<ChattingMessageList> createState() => _ChattingMessageListState();
+}
+
+class _ChattingMessageListState extends State<ChattingMessageList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: )
+  }
+}
---
+++ lib/view/chatting_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ChattingPage extends StatefulWidget {
+  const ChattingPage({super.key});
+
+  @override
+  State<ChattingPage> createState() => _ChattingPageState();
+}
+
+class _ChattingPageState extends State<ChattingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Icon(Icons.arrow_back_ios_new),
+              Text('이굠댕 바보',style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800 )),
+              SizedBox()
+            ],
+          ),
+          SizedBox(height: 10),
+          Divider(),
+          
+        ],
+      ),),
+    );
+  }
+}
